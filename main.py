6358c9fcb613642b3763f19448e463326f145845
WIP03 <41729427+WIP03@users.noreply.github.com>
2020-08-19 14:53:29 +0100

    Added Pause Menu
    Added a basic pause menu, contains no buttons or special features at current time.

--- main.py
+++ main.py
@@ -32,6 +32,7 @@ class Game():
     def __init__(self):
         #Initialises pygame#
         pg.init()
+        pg.font.init()
 
         #Initialisation of global constants#
         self.screenWidth = 856
@@ -39,6 +40,7 @@ class Game():
         self.tileSize = 36
         self.gameFPS = 60
         self.delta = 1.0
+        self.antialiasing = True
 
         #Sets up game window and game clock#
         self.screen = pg.display.set_mode((self.screenWidth, self.screenHeight))
@@ -49,8 +51,18 @@ class Game():
         self.all_sprites = pg.sprite.Group()
         self.all_tiles = pg.sprite.Group()
 
+        self.setupGame()
+
+    def setupGame(self):
         #Loads gameboard from textfile via map class#
         self.map = gameMap.Map(self,"Maps\TestMap.txt")
+
+        #Creats transparent overlays for background#
+        self.transOverlayLight = pg.Surface(self.screen.get_size()).convert_alpha()
+        self.transOverlayLight.fill(( 0, 0, 0, 120))
+
+        self.transOverlayDark = pg.Surface((self.screenWidth /  4, self.screenHeight)).convert_alpha()
+        self.transOverlayDark.fill(( 0, 0, 0, 60))
 
         #Draws gameboard by placing tiles in set locations#
         for x in range(0, self.map.tilesX):
@@ -68,6 +80,11 @@ class Game():
         #Creates map view#
         self.view = gameMap.MapView(self.map.pixelX, self.map.pixelY)
 
+    def createText(self, font, size, text, colour, position):
+        #Funtion used to create text easily ingame#
+        myfont = pg.font.SysFont(font, size)
+        textsurface = myfont.render(text, self.antialiasing, colour)
+        self.screen.blit(textsurface, position)
     
     def drawGame(self):
         #Draws all content in the window including sprites in the designated map view#
@@ -99,6 +116,16 @@ class Game():
         self.player.rect = self.player.image.get_rect(center=self.player.rect.center)
         self.player.image.blit(self.player.image, self.player.rect)
         pass
+
+    def mainMenu(self):
+        pass
+
+    def mapSelection(self):
+        pass
+
+    def options(self):
+        pass
+    
     
     def gameLoop(self):
         #Initialises key values before main game loop.#
@@ -106,6 +133,8 @@ class Game():
         self.mousex = 0
         self.mousey = 0
         self.angle = 0
+
+        self.paused = False
 
         #Main game loop#
         while self.gameRunning:
@@ -120,16 +149,37 @@ class Game():
                 #Updates current mouse poistion once pygame detects mouse movement#
                 if event.type == pg.MOUSEMOTION:
                     self.mousex, self.mousey = event.pos
+
+                if event.type == pg.KEYDOWN:
+                    #Checks to see if the player wants to acccess the pause menu#
+                    if event.key == pg.K_ESCAPE:
+                        self.paused = not self.paused
                     
             #Sets delta to current game tick rate (in this case based on frame rate)#
             self.delta = self.clock.tick(self.gameFPS)
+
+            if self.paused == False:
+                #Runs update code for player rotation, sprites, game graphics and the game window view in general#
+                self.gameHandler()
+                self.all_sprites.update(self.delta)
+                self.view.update(self, self.player)
+                self.drawGame()
+
+            else:
+                #Runs the games pause menu stoping all game updates#
+                self.pauseMenu()
             
-            #Runs update code for player rotation, sprites, game graphics and the game window view in general#
-            self.gameHandler()
-            self.all_sprites.update(self.delta)
-            self.view.update(self, self.player)
-            self.drawGame()
             pg.display.flip()
+
+
+    def pauseMenu(self):
+        #Draws the game screen in place every frame#
+        self.drawGame()
+
+        #Draws all ui elements for the pause screen#
+        self.screen.blit(self.transOverlayLight, (0,0))
+        self.screen.blit(self.transOverlayDark, (0,0))
+        self.createText('baskervilleoldface', 30, 'Paused', (255,0,0), (25,40))
         
     
 #Main game initialisation, loop and game end when window is closed#
